573b20dcb853457ecf58aa6424f4db1845e929a7
knubbe <knubbe1982@gmail.com>
2021-05-16 21:31:12 +0200

    find a coach - vuejs spa

---
+++ src/pages/coaches/CoachRegistration.vue
@@ -0,0 +1,25 @@
+<template>
+  <section>
+    <base-card>
+      <h2>Register as a coach now!</h2>
+      <coach-from @save-data="saveData"></coach-from>
+    </base-card>
+  </section>
+</template>
+
+<script>
+import CoachFrom from '../../components/coaches/CoachForm.vue';
+
+export default {
+  components: {
+    CoachFrom,
+  },
+
+  methods: {
+    saveData(data) {
+      this.$store.dispatch('coaches/registerCoach', data);
+      this.$router.replace('/coaches');
+    },
+  },
+};
+</script>
